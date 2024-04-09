19e9126d1610b910f9cce16693a41219a1539973
Prashant Kumar <47846896+prashant-01@users.noreply.github.com>
2024-04-10 00:54:03 +0530

    Time: 11 ms (58.30%) | Memory: 16.2 MB (89.24%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -19,37 +19,13 @@ public:
         vector<string>ans;
         unordered_map<string , int>mp;
         unordered_map<string , int>::iterator itr , itr1;
-        for(const auto& word : words)mp[word]++;
-        // for(int i=0 ; i<words.size() ; i++)mp[words[i]]++;
         priority_queue<Node* , vector<Node*> , Compare>minHeap;
+
+        for(const auto& word : words)mp[word]++;
         for(const auto& entry : mp){
             Node* newNode = new Node(entry.first , entry.second);
             minHeap.push(newNode);
         }
-        // for(itr = mp.begin() ; itr != mp.end() ; itr++){
-        //     if(k > 0){
-        //         Node* newNode = new Node(itr->first , itr->second);
-        //         minHeap.push(newNode);
-        //         k--;
-        //     }
-        //     else break;
-        // }
-        // for(itr1 = itr ; itr1 != mp.end() ; itr1++){
-        //     Node* top = minHeap.top();
-        //     cout << top->freq << " ";
-        //     if(itr1->second > top->freq){
-        //         minHeap.pop();
-        //         Node* newNode = new Node(itr1->first , itr1->second);
-        //         minHeap.push(newNode);
-        //     }
-        //     else if(itr1->second == top->freq){
-        //         if( itr->first < top->s ){
-        //             minHeap.pop();
-        //             Node* newNode = new Node(itr1->first , itr1->second);
-        //             minHeap.push(newNode);
-        //         }
-        //     }
-        // }
         while(!minHeap.empty() && k-- > 0){
             Node* top = minHeap.top();
             ans.push_back(top->s);
