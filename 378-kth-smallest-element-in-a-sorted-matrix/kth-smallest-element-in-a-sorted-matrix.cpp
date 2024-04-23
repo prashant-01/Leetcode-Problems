7ba34522591289e9f08b935af84c9635cd978fce
Prashant Kumar <47846896+prashant-01@users.noreply.github.com>
2024-04-23 20:17:09 +0530

    Time: 22 ms (72.66%) | Memory: 17.7 MB (45.86%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,22 +1,37 @@
+// class Solution {
+// public:
+//     typedef pair<int , int>P;
+//     int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         int n = matrix.size() , ans;
+//         auto compare = [matrix](P a , P b){
+//             return matrix[a.first][a.second] > matrix[b.first][b.second];
+//         };
+//         priority_queue<P , vector<P>, decltype(compare)>minHeap(compare);
+//         for(int i=0 ; i<n ; i++)minHeap.push({ i , 0 });
+//         while(k--){
+//             P p = minHeap.top();
+//             minHeap.pop();
+//             ans=matrix[p.first][p.second];
+//             if(p.second+1 < n)minHeap.push({ p.first , p.second + 1 });
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    typedef pair<int , pair<int , int>>P;
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n = matrix.size();
-        set<P>s;
-        set<P>::iterator itr;
-        s.insert({ matrix[0][0] , { 0 , 0 } });
-        while(k > 1 && !s.empty()){
-            itr = s.begin();
-            P p = *itr;
-            s.erase(itr);
-            if(p.second.second + 1 < n)
-                s.insert({ matrix[p.second.first][p.second.second + 1] , { p.second.first , p.second.second + 1 } });
-            if(p.second.first + 1 < n)
-                s.insert({ matrix[p.second.first + 1][p.second.second] , { p.second.first + 1 , p.second.second } });
-            k--;
+        
+        priority_queue<int>max_h;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[i].size();j++)
+            {
+                max_h.push(matrix[i][j]);
+                if(max_h.size()>k)
+                max_h.pop();
+            }
         }
-        itr = s.begin();
-        return (*itr).first;
+        return max_h.top();
     }
 };
